543bb42bf8c6096af1b16e6741a823601fa1dbdf
Victor Nogueira <victornogu80@gmail.com>
2022-08-14 22:42:31 -0300

    Removendo código com erro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,13 +30,6 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        view()->composer("master.master", function ($view) {
-            $user = User::find(Auth::id());
-            $unity = $user->unities();
-
-            $view->with("user", $user);
-        });
-
         Blade::component(Alert::class, "alert");
     }
 }
